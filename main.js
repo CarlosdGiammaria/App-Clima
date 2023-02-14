bdc551bab19553c1b348d85abbe28d18fed4465a
Carlos Giammaria <carlosgiammaria29@gmail.com>
2023-02-14 16:34:45 -0500

    Implementa el loader de la pagina

--- main.js
+++ main.js
@@ -3,18 +3,23 @@ const containerWeather = document.querySelector(".js-weather-city");
 const apiKey = "3d169feda7a066ff2b90a81de6a1f71e";
 
 window.addEventListener("DOMContentLoaded", () => {
+  loader();
   getCoords();
+  
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+
     let formData = new FormData(e.target);
     let city = formData.get("city");
+
     !city ? alert("BY ENTER A VALID VALUE") : getWeather(city);
   });
 });
 
 function getWeather(query, coords = {}) {
   const url = Object.keys(coords).length
-    ? `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`
+
+    ? `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&lang=sp&appid=${apiKey}`
     : `https://api.openweathermap.org/data/2.5/weather?q=${query}&lang=sp&appid=${apiKey}`;
 
   fetch(url)
@@ -30,27 +35,28 @@ function getWeather(query, coords = {}) {
 }
 
 function showWheather(country) {
+
   const [weather] = country.weather;
   const { speed } = country.wind;
   const { temp } = country.main;
-  containerWeather.innerHTML = `
 
+  template = `
     <div class="container__weather-city ">
       <div class="weather-city__details">
         <span class="big-text"> ${toCelsius(temp)}º </span>
         <div class="wind">
           <span class="opaque-text">  ${weather.description}</span>
           <span class="opaque-text"><img src="./image/wind.png" class="small-icon"> ${speed}</span>
-          <span>${country.name}</span>
+          <span class="bold-text">${country.name}</span>
         </div>
       </div>
       <div class="weather-city__img">
         <img src="" alt="" class="js-img"/>
-        <span>${weather.main}</span>
+        <span class="bold-text">${weather.main}</span>
       </div>
     </div>
-    
   `;
+  containerWeather.innerHTML = template;
 }
 
 function toCelsius(kelvin) {
@@ -90,4 +96,10 @@ function getCoords() {
       getWeather("", { lat, lon });
     });
   }
+}
+function loader() {
+  loader = `
+  <div class="spinner j-spinner"></div>
+`;
+  containerWeather.innerHTML = loader;
 }
